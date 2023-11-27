59c615917f105f5c648a8faf67a97bd5b04d0f07
suman.mathad <suman.mathad@oracle.com>
2023-11-27 23:19:14 +0530

    updates to js basics

---
+++ sessions-content/JavaScript_basics/arraysBasics.js
@@ -0,0 +1,31 @@
+///ordered collections
+
+let days= [];
+ console.log(typeof days);
+ console.log(days);
+ console.log(days.length);
+
+ days=['sun','Mon'];
+
+ console.log(typeof days);
+ console.log(days);
+ console.log(days.length);
+
+//Splice
+let colors = ['red', 'blue', 'green'];
+colors.splice(1,0,'violet');
+console.log(colors);
+
+//sort
+console.log("********Sort Methods**********");
+//converts to string and compares UTF-18 values
+
+console.log(colors);
+colors.sort();
+console.log(colors);
+
+//Reference types and Equality Testing
+
+
+
+
---
+++ sessions-content/JavaScript_basics/objectLiterals.js
@@ -0,0 +1,35 @@
+let product = {
+  name:'Gummy Bears',
+  inStock:true,
+  price:1.99,
+  flavors:['grape','apple','cherry'],
+  true:'no', 
+  null:'yes'
+
+}
+
+console.log(product["inStock"]);
+
+console.log(product[true]);
+console.log(product['true']);
+console.log(product[null]);
+console.log(product['null']);
+
+// works as all keys are converted to strings
+
+console.log(product["inStock"]);
+console.log(product.inStock);
+
+
+
+
+const restaurant = {
+  name: 'Ichiran Ramen',
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: 'Brooklyn',
+  state: 'NY',
+  zipcode: '11206',
+}
+
+let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}` 
+console.log(fullAddress)
